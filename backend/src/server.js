ab02d7a38f6e28cfff0799f5490bf8eab0609206
Chia-Wen Lu <qwe9887476@gmail.com>
2023-01-05 10:55:51 +0800

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -33,7 +33,6 @@ mongoose.connect(
 )
 
 if(mongoose.connection.readyState == 2){
-    mongoose.set("strictQuery", false);
     console.log('DB online')
     routes(app)
     app.listen(port, () => {
